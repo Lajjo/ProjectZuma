6fc03d1ec2cdd17bd9c05dfcda9e7c0a034f8eb1
Lajjo <Lajjo1992@gmail.com>
2013-02-01 18:10:19 +0100

    Update LevelEngine.cpp

--- LevelEngine.cpp
+++ LevelEngine.cpp
@@ -7,7 +7,7 @@
 
 LevelEngine::LevelEngine(GasCloud &cloud,sf::Sprite &nCastSprite) : gasCloud(cloud) , castSprite(nCastSprite)
 {
-  level.loadFromFile("../Levels/feest.png");
+	level.loadFromFile("../Levels/feest.png");
 	timer.restart();
 	healCast.restart();
 	for(unsigned i = 0; i < level.getSize().x; i++)
@@ -23,31 +23,36 @@ void LevelEngine::update(Character& player, KitStack& ks, TubeStack& ts, Invento
 {
 	sf::IntRect area;
 	sf::IntRect rectplayer = player.getIntrect();
-	for(unsigned i = (player.getPosition().x / 32) - 1; i < (player.getPosition().x / 32) + 5; i++)
+
+	for(unsigned i = static_cast <int>(player.getPosition().x / 32) - 4; i < static_cast <int>(player.getPosition().x / 32) + 6; i++)
 	{
-		for(unsigned j = (player.getPosition().y / 32) - 1; j < (player.getPosition().y / 32) + 5; j++)
+		for(unsigned j = static_cast <int>(player.getPosition().y / 32) - 4; j < static_cast <int>(player.getPosition().y / 32) + 8; j++)
 		{
-			if(rectplayer.intersects(sf::IntRect(i * 32, j * 32, 32, 32), area) && level.getPixel(i, j) != My::Color::Space && level.getPixel(i, j) != My::Color::Poison && level.getPixel(i, j) != My::Color::Gas && level.getPixel(i, j) != My::Color::Ladder)
+			if(player.getIntrect().intersects(sf::IntRect(i * 32, j * 32, 32, 32), area) && level.getPixel(i, j) != My::Color::WeakRockLeftEdge && level.getPixel(i, j) != My::Color::WeakRockLowLeftCorner && level.getPixel(i, j) != My::Color::WeakRockLowRightCorner && level.getPixel(i, j) != My::Color::Space && level.getPixel(i, j) != My::Color::Poison && level.getPixel(i, j) != My::Color::Gas && level.getPixel(i, j) != My::Color::Ladder)
 			{
 				if(area.width < area.height)
 				{
-					if(player.getVelocity().x > 0)
-					{
-						player.setPosition(player.getPosition() - sf::Vector2f(static_cast <float>(area.width), 0));
-					}
 					if(player.getVelocity().x < 0)
-					{
-						player.setPosition(player.getPosition() + sf::Vector2f(static_cast <float>(area.width), 0)); 
-					}
-					player.setVelocity(sf::Vector2f(0, player.getVelocity().y));
+						player.setPosition(player.getPosition() + sf::Vector2f(area.width, 0));
+					else if(player.getVelocity().x > 0)
+						player.setPosition(player.getPosition() - sf::Vector2f(area.width, 0));
+					else if(player.getVelocity().x == 0)
+						if(player.getVelocity().y < 0)
+							player.setPosition(player.getPosition() + sf::Vector2f(0, area.height)), player.setVelocity(sf::Vector2f(player.getVelocity().x, 0));
+						else if(player.getVelocity().y > 0)
+							player.setPosition(player.getPosition() - sf::Vector2f(0, area.height));
 				}
-				if(area.width > area.height)
+				else if(area.width > area.height)
 				{
-					if(player.getVelocity().y > 0)
-						player.setPosition(player.getPosition() - sf::Vector2f(0, static_cast <float>(area.height)));
 					if(player.getVelocity().y < 0)
-						player.setPosition(player.getPosition() + sf::Vector2f(0, static_cast <float>(area.height)));
-					player.setVelocity(sf::Vector2f(player.getVelocity().x, 0));
+						player.setPosition(player.getPosition() + sf::Vector2f(0, area.height)), player.setVelocity(sf::Vector2f(player.getVelocity().x, 0));
+					else if(player.getVelocity().y > 0)
+						player.setPosition(player.getPosition() - sf::Vector2f(0, area.height));
+					else if(player.getVelocity().y == 0)
+						if(player.getVelocity().x < 0)
+							player.setPosition(player.getPosition() + sf::Vector2f(area.width, 0));
+						else if(player.getVelocity().x > 0)
+							player.setPosition(player.getPosition() - sf::Vector2f(area.width, 0));
 				}
 				if(area.width == area.height)
 				{
@@ -78,23 +83,25 @@ void LevelEngine::update(Character& player, KitStack& ks, TubeStack& ts, Invento
 					}
 				}
 			}
-			
-			if(sf::IntRect(i * 32, j * 32, 32, 32).contains(sf::Vector2i(rectplayer.left + 32, rectplayer.top + rectplayer.height + 2)) && (level.getPixel(i, j) == My::Color::Space || level.getPixel(i, j) == My::Color::Poison || level.getPixel(i, j) == My::Color::Gas))
+			if(!player.getOnLadder())
+			if(sf::IntRect(player.getIntrect().left + 5, player.getIntrect().top, player.getIntrect().width - 10, player.getIntrect().height).intersects(sf::IntRect(i * 32, j * 32 - 2, 32, 32)) && (level.getPixel(i, j) == My::Color::Space || level.getPixel(i, j) == My::Color::Poison || level.getPixel(i, j) == My::Color::Gas))
 				player.setInAir(true);
-			else if(sf::IntRect(i * 32, j * 32, 32, 32).contains(sf::Vector2i(rectplayer.left + 32, rectplayer.top + rectplayer.height + 2)) && (level.getPixel(i, j) != My::Color::Space || level.getPixel(i, j) != My::Color::Poison || level.getPixel(i, j) != My::Color::Gas))
+			else if(sf::IntRect(player.getIntrect().left + 5, player.getIntrect().top, player.getIntrect().width - 10, player.getIntrect().height).intersects(sf::IntRect(i * 32, j * 32 - 2, 32, 32)) && (level.getPixel(i, j) != My::Color::Space || level.getPixel(i, j) != My::Color::Poison || level.getPixel(i, j) != My::Color::Gas))
 			{
 				player.setInAir(false);
-				player.changeJump();
+				player.setVelocity(sf::Vector2f(player.getVelocity().x, 0));
+				player.setJump(false);
 			}
-			if(player.getIntrect().contains(sf::Vector2i(i * 32 + 16, j * 32)) && level.getPixel(i,j) == My::Color::Ladder){
+			if(player.getIntrect().contains(sf::Vector2i(i * 32 + 32, j * 32)) && level.getPixel(i,j) == My::Color::Ladder){
 				player.setOnLadder(true);
 				player.setInAir(false);
-
 			}
-			else if(player.getIntrect().contains(sf::Vector2i(i * 32 + 16, j * 32)) && level.getPixel(i,j) != My::Color::Ladder)
+			else if(player.getIntrect().contains(sf::Vector2i(i * 32 + 32, j * 32)) && level.getPixel(i,j) != My::Color::Ladder)
 				player.setOnLadder(false);
 		}
 	}
+	if(player.getOnLadder())
+		player.setInAir(false);
 	if(player.getO2() <= 0){
 		player.setO2(0);
 	}
@@ -105,23 +112,21 @@ void LevelEngine::update(Character& player, KitStack& ks, TubeStack& ts, Invento
 	if(timer.getElapsedTime().asMilliseconds() > 50){
 		timer.restart();
 
-		if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+35)) && sf::Mouse::isButtonPressed(sf::Mouse::Right) && static_cast <int>(player.getO2()) > 0 && player.getMoveState())
+		if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+15)) && sf::Mouse::isButtonPressed(sf::Mouse::Right) && static_cast <int>(player.getO2()) > 0 && player.getMoveState())
 			player.setO2(player.getO2()-0.15f);
-		else if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+35)) && sf::Mouse::isButtonPressed(sf::Mouse::Right) && static_cast <int>(player.getO2()) > 0)
+		else if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+15)) && sf::Mouse::isButtonPressed(sf::Mouse::Right) && static_cast <int>(player.getO2()) > 0)
 			player.setO2(player.getO2()-0.05f);
-		else if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+35)))
+		/*else if(insideGas && player.getMoveState())
+			player.setHP(player.getHP()-6);*/
+		else if(gasCloud.checkIfInsideGas(static_cast <int>(player.getPosition().x+24), static_cast <int>(player.getPosition().y+15)))
 			player.setHP(player.getHP()-0.2f);
 	}
 	if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-		if(inv.checkIfPressed(mousePos.x,mousePos.y)>0){
+		if(inv.checkIfPressed(mousePos.x,mousePos.y) > 0){
 			player.setActiveItem(inv.checkIfPressed(mousePos.x,mousePos.y));
 		}
 	}
-
-	//std::cout << player.getActiveItem() << std::endl;
-
-
-	if(player.getHP()<100 && iks.getStackSize()>0 && sf::Mouse::isButtonPressed(sf::Mouse::Left)&&player.getCanMove()&&player.getActiveItem() == 3){
+	if(player.getHP()<100 && iks.getStackSize()>0 && sf::Mouse::isButtonPressed(sf::Mouse::Left)&&player.getCanMove()&&player.getActiveItem() == 4){
 
 		castSprite.setScale(healCast.getElapsedTime().asMilliseconds()/16.5f, 2.f);
 		if(healCast.getElapsedTime().asSeconds() > 2){
@@ -136,7 +141,7 @@ void LevelEngine::update(Character& player, KitStack& ks, TubeStack& ts, Invento
 		castSprite.setScale(0,2);
 	}
 
-	if(player.getO2()<20 && sf::Mouse::isButtonPressed(sf::Mouse::Left) && player.getCanMove() && player.getActiveItem() == 4){
+	if(player.getO2()<20 && sf::Mouse::isButtonPressed(sf::Mouse::Left) && player.getCanMove() && player.getActiveItem() == 5){
 		player.setO2(its.useTube(player.getO2()));
 		if(its.getCurrentTubeO2()<=0)
 			its.deleteTube();			
